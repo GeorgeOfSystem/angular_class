1b3457a56eab6e97418ef3299a92e44fd9e5546f
GeorgeOfSystem <georgeilr99@gmail.com>
2020-08-09 11:09:19 -0400

    my-first-observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-class';
+
+  ngOnInit(){
+    const asynArray = from([1,2,3,4,5,6]);
+    asynArray.subscribe(s => console.log('item', s));
+
+    const mouseMovement = fromEvent(document, "mousemove");
+    mouseMovement.subscribe(s => console.log('event',s.clientX + ', ' + s.clientY));
+  }
 }
