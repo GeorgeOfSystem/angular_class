4c36f9cca0c53584343f2f9842f743bd06e8047e
GeorgeOfSystem <georgeilr99@gmail.com>
2020-08-15 22:29:27 -0400

    my First Pipe

--- src/app/myPipes/my-first-pipe.pipe.ts
+++ src/app/myPipes/my-first-pipe.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class MyFirstPipePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return 'hi' + value;
+    return 'hi ' + value;
   }
 
 }
