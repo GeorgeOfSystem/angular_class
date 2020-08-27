cd8b2331dda33d8458e0bca87294333246766c72
GeorgeOfSystem <georgeilr99@gmail.com>
2020-08-26 22:01:24 -0400

    Child in router module

--- src/app/myModules/user-router/user-router-routing.module.ts
+++ src/app/myModules/user-router/user-router-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './users/users.component';
+import { User1Component } from './user1/user1.component';
+import { User2Component } from './user2/user2.component';
 
 const routes: Routes = [
-  {path:'', component: UsersComponent}
+  {path:'',
+  component: UsersComponent,
+  children: [
+    {path: 'user1', component: User1Component },
+    {path: 'user2', component: User2Component }
+  ]
+  }
 ];
 
 @NgModule({
--- src/app/myModules/user-router/user-router.module.ts
+++ src/app/myModules/user-router/user-router.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserRouterRoutingModule } from './user-router-routing.module';
 import { UsersComponent } from './users/users.component';
+import { User1Component } from './user1/user1.component';
+import { User2Component } from './user2/user2.component';
 
 
 @NgModule({
   declarations: [
-    UsersComponent
+    UsersComponent,
+    User1Component,
+    User2Component
   ],
   imports: [
     CommonModule,
