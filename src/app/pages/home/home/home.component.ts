bcc0bee31a4ab3e861cf1c4805ee0dea54db347d
GeorgeOfSystem <georgeilr99@gmail.com>
2020-08-30 15:39:59 -0400

    Cards in Home

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -7,6 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  products = [
+    {
+      description: 'Salteñas de las potosinas',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'salteñas'
+    },
+    {
+      description: 'Salteñas de las SuperSalteñas',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'salteñas'
+    },
+    {
+      description: 'Salteñas de las Castores',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'salteñas'
+    },
+    {
+      description: 'Salteñas de las Paceñas',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'salteñas'
+    }
+  ];
+
+
   constructor() { }
 
   ngOnInit(): void {
