4ffc5619d12d77ab7c2173e280d392a0ccacd200
GeorgeOfSystem <georgeilr99@gmail.com>
2020-08-30 15:44:05 -0400

    Preparing enviroment for Login and Pages Routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
+/*const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
   {path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule)},
   {path: 'list', loadChildren: () => import('./pages/list/list.module').then(m => m.ListModule)}
+];*/
+
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+  {path: 'pages', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule)}
 ];
 
 @NgModule({
