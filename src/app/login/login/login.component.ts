15b732ad891f9e013b95b0871159c1a2d5ce631d
GeorgeOfSystem <georgeilr99@gmail.com>
2020-09-03 09:29:49 -0400

    Reactive Form Examples

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,7 @@ export class LoginComponent implements OnInit {
 
   sw = true;
 
-  constructor() { }
+  constructor( private router : Router ) { }
 
   ngOnInit(): void {
   }
@@ -24,5 +25,6 @@ export class LoginComponent implements OnInit {
 
   onLogin2 (form) : void {
     console.log('Variables: ',form.value);
+    this.router.navigate(['/pages']);
   }
 }
--- src/app/pages/admin/admin/admin.component.ts
+++ src/app/pages/admin/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  productForm : FormGroup;
+
+  //nameControl = new FormControl();
+
+  constructor(private formBuilder : FormBuilder) { }
 
   ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      description: '',
+      imageUrl: '',
+      ownerId: '',
+      price: '',
+      tittle: ''
+    });
   }
-
+  
+  /*
+  onSend(){
+    console.log('value', this.nameControl.value);
+  }
+  */
+  onSend2(){
+    console.log('Form group', this.productForm.value);
+  }
 }
