775499b1f18e26900b0531ec769a4a63bd5a7994
GeorgeOfSystem <georgeilr99@gmail.com>
2020-09-17 11:16:53 -0400

    Configurating Dependencies and Enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  app: {
+    apiBaseUrl: ''
+  },
+  auth: {
+    apiBaseUrl: 'https://identitytoolkit.googleapis.com',
+    key: ''
+  }
 };
